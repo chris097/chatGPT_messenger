cb92cc5ee4769d89998a41dfaba62ca7245a925a
chris097 <chrisfidel.international@gmail.com>
2023-03-01 14:05:57 +0100

    added session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,8 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import Login from '@/components/Login';
 
-export default function App({ Component, pageProps:{ session, ...pageProps} }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  console.log('seesion:', session)
   return <SessionProvider session={session}>
     {!session ? <Login /> : <Component {...pageProps} />}
   </SessionProvider>
